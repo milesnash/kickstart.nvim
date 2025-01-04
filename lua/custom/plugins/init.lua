31f627b37cc392c87bedf3b75de5302e14008deb
milesnash <miles@milesnash.com>
2025-01-04 14:27:19 +0000

    Preferred default indentation

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,12 +3,21 @@
 --
 -- See the kickstart.nvim README for more information
 
+-- Preferred indentation defaults
+-- Note, these should be overridden by more specific rules according to auto-setting
+-- e.g. ftplugins and editorconfig files
+-- See :help auto-setting
+vim.o.expandtab = true
+vim.o.shiftwidth = 4
+
 -- Pretty symbols for diagnostics
 vim.fn.sign_define('DiagnosticSignError', { text = ' ', texthl = 'DiagnosticSignError' })
 vim.fn.sign_define('DiagnosticSignWarn', { text = ' ', texthl = 'DiagnosticSignWarn' })
 vim.fn.sign_define('DiagnosticSignInfo', { text = ' ', texthl = 'DiagnosticSignInfo' })
 vim.fn.sign_define('DiagnosticSignHint', { text = '󰌵', texthl = 'DiagnosticSignHint' })
 
+-- Allow project-specific nvim config files to be read
+-- e.g. .nvim.lua at the project's root
 vim.o.exrc = true
 
 return {}
