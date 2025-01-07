030f84b6392c1c6ed496ed538ab14110574f49da
Miles Nash <miles.nash@sky.uk>
2025-01-07 10:34:32 +0000

    Trying tomorrow-night

--- init.lua
+++ init.lua
@@ -874,14 +874,15 @@ require('lazy').setup({
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     -- 'folke/tokyonight.nvim',
-    'EdenEast/nightfox.nvim',
+    -- 'EdenEast/nightfox.nvim',
     -- 'sainnhe/sonokai',
+    'squarefrog/tomorrow-night.vim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'carbonfox'
+      vim.cmd.colorscheme 'tomorrow-night'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
