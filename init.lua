35d655f83b87b6d43d13ec500a09ccb3f856bfae
Miles Nash <miles.nash@sky.uk>
2025-05-31 13:56:15 +0100

    Enable pyright + remove unneeded tokyo stuff

--- init.lua
+++ init.lua
@@ -675,7 +675,7 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         gopls = {},
-        -- pyright = {},
+        pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -920,13 +920,6 @@ require('lazy').setup({
     'squarefrog/tomorrow-night.vim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
-        },
-      }
-
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
